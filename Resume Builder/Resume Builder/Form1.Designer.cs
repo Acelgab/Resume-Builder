e1afe8ca7cf227e5cbc42ec3987cfece3eb64662
Acel Margallo <gabrielleacel@gmail.com>
2022-07-06 21:50:30 +0800

    Resume Builder Design

---
+++ Resume Builder/Resume Builder/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Resume_Builder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbxFullName = new System.Windows.Forms.TextBox();
+            this.txtbxContact = new System.Windows.Forms.TextBox();
+            this.txtbxEmail = new System.Windows.Forms.TextBox();
+            this.txtbxAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnBasicInfo = new System.Windows.Forms.Button();
+            this.btnProfessional = new System.Windows.Forms.Button();
+            this.btnEduc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(79, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Builder";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Resume_Builder.Properties.Resources.image_2022_07_06_210207199_removebg_preview;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(690, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(578, 468);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Full Name:";
+            // 
+            // txtbxFullName
+            // 
+            this.txtbxFullName.Location = new System.Drawing.Point(294, 82);
+            this.txtbxFullName.Name = "txtbxFullName";
+            this.txtbxFullName.Size = new System.Drawing.Size(272, 22);
+            this.txtbxFullName.TabIndex = 4;
+            // 
+            // txtbxContact
+            // 
+            this.txtbxContact.Location = new System.Drawing.Point(818, 125);
+            this.txtbxContact.Name = "txtbxContact";
+            this.txtbxContact.Size = new System.Drawing.Size(172, 22);
+            this.txtbxContact.TabIndex = 5;
+            // 
+            // txtbxEmail
+            // 
+            this.txtbxEmail.Location = new System.Drawing.Point(572, 125);
+            this.txtbxEmail.Name = "txtbxEmail";
+            this.txtbxEmail.Size = new System.Drawing.Size(240, 22);
+            this.txtbxEmail.TabIndex = 6;
+            // 
+            // txtbxAddress
+            // 
+            this.txtbxAddress.Location = new System.Drawing.Point(218, 125);
+            this.txtbxAddress.Name = "txtbxAddress";
+            this.txtbxAddress.Size = new System.Drawing.Size(348, 22);
+            this.txtbxAddress.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(815, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contact Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(569, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(486, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "BASIC INFORMATION\r\n";
+            // 
+            // btnBasicInfo
+            // 
+            this.btnBasicInfo.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnBasicInfo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBasicInfo.Location = new System.Drawing.Point(-4, 55);
+            this.btnBasicInfo.Name = "btnBasicInfo";
+            this.btnBasicInfo.Size = new System.Drawing.Size(132, 23);
+            this.btnBasicInfo.TabIndex = 12;
+            this.btnBasicInfo.Text = "Basic Information";
+            this.btnBasicInfo.UseVisualStyleBackColor = false;
+            // 
+            // btnProfessional
+            // 
+            this.btnProfessional.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnProfessional.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnProfessional.Location = new System.Drawing.Point(-4, 82);
+            this.btnProfessional.Name = "btnProfessional";
+            this.btnProfessional.Size = new System.Drawing.Size(165, 23);
+            this.btnProfessional.TabIndex = 13;
+            this.btnProfessional.Text = "Professional Experience";
+            this.btnProfessional.UseVisualStyleBackColor = false;
+            // 
+            // btnEduc
+            // 
+            this.btnEduc.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnEduc.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEduc.Location = new System.Drawing.Point(-4, 111);
+            this.btnEduc.Name = "btnEduc";
+            this.btnEduc.Size = new System.Drawing.Size(89, 23);
+            this.btnEduc.TabIndex = 14;
+            this.btnEduc.Text = "Education\r\n";
+            this.btnEduc.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.Controls.Add(this.btnEduc);
+            this.Controls.Add(this.btnProfessional);
+            this.Controls.Add(this.btnBasicInfo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbxAddress);
+            this.Controls.Add(this.txtbxEmail);
+            this.Controls.Add(this.txtbxContact);
+            this.Controls.Add(this.txtbxFullName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resume Builder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbxFullName;
+        private System.Windows.Forms.TextBox txtbxContact;
+        private System.Windows.Forms.TextBox txtbxEmail;
+        private System.Windows.Forms.TextBox txtbxAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnBasicInfo;
+        private System.Windows.Forms.Button btnProfessional;
+        private System.Windows.Forms.Button btnEduc;
+    }
+}
+
